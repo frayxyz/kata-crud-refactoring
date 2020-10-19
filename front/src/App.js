22aa465c113aa74d54f11bee613a6d08c43f4486
fray <fdmontoya0@misena.edu.co>
2020-10-18 19:15:53 -0500

    refactorizacion reducer en archivo reducer.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import { Button } from '@material-ui/core';
 import React, { useContext, useReducer, useEffect, useRef, useState, createContext } from 'react';
+import { reducer } from './reducer';
 import styles from './style/styles';
 
 
@@ -156,43 +157,6 @@ const List = () => {
 }
 
 
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'update-item':
-      const todoUpItem = state.todo;
-      const listUpdateEdit = todoUpItem.list.map((item) => {
-        if (item.id === action.item.id) {
-          return action.item;
-        }
-        return item;
-      });
-      todoUpItem.list = listUpdateEdit;
-      todoUpItem.item = {};
-      return { ...state, todo: todoUpItem }
-    case 'delete-item':
-      const todoUpDelete = state.todo;
-      const listUpdate = todoUpDelete.list.filter((item) => {
-        return item.id !== action.id;
-      });
-      todoUpDelete.list = listUpdate;
-      return { ...state, todo: todoUpDelete }
-    case 'update-list':
-      const todoUpList = state.todo;
-      todoUpList.list = action.list;
-      return { ...state, todo: todoUpList }
-    case 'edit-item':
-      const todoUpEdit = state.todo;
-      todoUpEdit.item = action.item;
-      return { ...state, todo: todoUpEdit }
-    case 'add-item':
-      const todoUp = state.todo.list;
-      todoUp.push(action.item);
-      return { ...state, todo: { list: todoUp, item: {} } }
-    default:
-      return state;
-  }
-}
 
 const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
